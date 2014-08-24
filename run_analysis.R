040f482425057b78545074efbdc2f8b924b91005
aysla001 <aysla001@yahoo.com>
2014-08-24 17:44:14 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,22 +8,20 @@ BiometricReader <- function (){
   features_colv2<-features$V2
   headers<-t(features$V2)
   print (features)
-  write.table(headers, "C:/Users/Angel/Desktop/Stuff/Learning/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/Biometric.txt", sep="\t")
   features_t <- t(features)
   
   setwd("./test")
   print(dir())
     x_test<-read.table("X_test.txt", header = FALSE)
-    #print(head(x_test))
   
   setwd(directory)
   setwd("./train")
     x_train <-read.table("X_train.txt", header = FALSE)
-    #print (head(x_train))
-  print(dir())
+
+  x_test_train<-rbind (features_t,x_test, x_train)
+  x_test_train_mean_std <- x_test_train$tBodyAcc-mean()-X, x_test_train$tBodyAcc-mean()-Y, x_test_train$tBodyAcc-mean()-Z, x_test_train$tBodyAcc-std()-X, x_test_train$tBodyAcc-std()-Y, x_test_train$tBodyAcc-std()-Z
   
-  x_test_train<-rbind (x_test, x_train)
-  #print(head(x_test_train))
-setwd(directory)  
-#setwd("C:/Users/Angel/Desktop/Stuff/Learning/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset")
+  write.table(x_test_train_mean_std, c(directory, "/run_analysis.txt", sep="\t")
+  
+  setwd(directory)  
 }
