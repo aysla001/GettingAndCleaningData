89e877e76985719cf0302e5a08560c0de90139df
aysla001 <aysla001@yahoo.com>
2014-08-24 18:21:09 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-## created by Angel Ysla 20140821
+## created by Angel Ysla 20140824
 ## setwd("C:/Users/Angel/Desktop/Stuff/Learning/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset")
 
 BiometricReader <- function (){
@@ -11,12 +11,15 @@ BiometricReader <- function (){
   features_t <- t(features)
   
   setwd("./test")
-  print(dir())
     x_test<-read.table("X_test.txt", header = FALSE)
+    result_x_test<-read.table("subject_test.txt", header = FALSE)
+    x_test_result <- cbind(x_test, result_x_test)
   
   setwd(directory)
   setwd("./train")
     x_train <-read.table("X_train.txt", header = FALSE)
+    result_x_train <- read.table("subject_train.txt", header = FALSE)
+    x_train_result <-cbind(x_train, result_x_train)
 
   x_test_train<-rbind (features_t,x_test, x_train)
   x_test_train_mean_std <- x_test_train$tBodyAcc-mean()-X, x_test_train$tBodyAcc-mean()-Y, x_test_train$tBodyAcc-mean()-Z, x_test_train$tBodyAcc-std()-X, x_test_train$tBodyAcc-std()-Y, x_test_train$tBodyAcc-std()-Z
